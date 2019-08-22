0305d21af007db8c714e765173f8130b69d9d521
leon <leonrlr4@gmail.com>
2019-08-22 12:11:14 +0800

    settings

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framwork.authtoken',
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
